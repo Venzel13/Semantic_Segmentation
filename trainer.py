53d56f080b22d9e33e015465073726fbb0e3da30
Eduard Kustov <Venzel13@gmail.com>
2022-03-15 18:45:19 +0300

    move to GPUs

--- trainer.py
+++ trainer.py
@@ -9,7 +9,8 @@ from config import DIR_PATH
 model = LeafModule()
 data = LeafDataModule(DIR_PATH)
 trainer = pl.Trainer(
+    gpus=1,
     callbacks = [EarlyStopping('val_loss')],
-    log_every_n_steps = 1,
+    # log_every_n_steps = 1,
 )
 trainer.fit(model, data)

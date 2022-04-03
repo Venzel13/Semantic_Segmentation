574902bf80d6fdf4e1474df7684d393b04c69e7a
Eduard Kustov <Venzel13@gmail.com>
2022-04-03 19:30:41 +0300

    test

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
+import pytorch_lightning as pl
+
+from config import DIR_PATH
+from model import LeafModule
+from preproc import LeafDataModule
+
 #TODO trainer.test(model, data, ckpt_path='best')
 #TODO trainer.predict() ?!
+
+model = LeafModule.load_from_checkpoint('/lightning_logs/version_1/checkpoints/epoch=49-step=599.ckpt')
+data = LeafDataModule(DIR_PATH)
+
+
+trainer = pl.Trainer()
+trainer.test(model, data, ckpt_path='best')

14736a078369de5261ce7933c867262bafe01384
Eduard Kustov <Venzel13@gmail.com>
2022-04-04 11:57:08 +0300

    plotresult

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
 from os import listdir
 from typing import Literal
 
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 
 def get_img_mask_paths(dirname: str, fold: Literal['images', 'masks']) -> np.ndarray:
     assert fold in {'images', 'masks'}
     paths = dirname + fold + '/' + np.array(listdir(dirname + fold), dtype='object')
     return np.sort(paths)
+
+
+def plot_random_pred_mask(pred: torch.Tensor, mask: torch.Tensor) -> None:
+    i = np.random.choice(len(pred))
+    plt.subplot(121)
+    plt.title('true')
+    plt.imshow(mask[i])
+    plt.subplot(122)
+    plt.title('pred')
+    plt.imshow(pred[i])
